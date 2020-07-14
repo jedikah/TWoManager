fd2a638ea91463797e826797cb5a6df2bb89db71
jedikah <jekjedika@gmail.com>
2020-07-14 23:14:15 +0300

    nest run on child process

--- src-electron/main-process/child/release/index.ts
+++ src-electron/main-process/child/release/index.ts
@@ -1,9 +1,11 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+// const express = require('express');
+// const app = express();
+// const path = require('path');
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+import './two/src/main';
 
-app.listen(8080);
+// app.get('/app', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+// app.listen(3001);
