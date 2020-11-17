30e3986ed8b50916d7ca903e79c5efe89c5faaa7
Joel Olivier SIDY <joeloliviersidy@gmail.com>
2020-11-17 15:58:34 +0300

    feat(front): form add folder

--- Front/src/pages/Folder.vue
+++ Front/src/pages/Folder.vue
@@ -1,16 +1,19 @@
 <template>
   <q-page>
     <ListFolder />
+    <AddFolder />
   </q-page>
 </template>
 
 <script lang="ts">
 import ListFolder from '../components/folder/ListFolder.vue';
+import AddFolder from '../components/folder/AddFolder.vue';
 
 export default {
   name: 'Folder',
   components: {
-    ListFolder
+    ListFolder,
+    AddFolder
   }
 };
 </script>
